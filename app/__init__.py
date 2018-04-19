4f6040654739650d50d7b45998ff1d55f94ba3d6
Charles K <kcharles52@gmail.com>
2018-04-19 12:54:45 +0300

    home module

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from .views.home import home #import home blue print
 
-app = Flask(__name__)
+
+app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
+app.config.from_pyfile('config.py') #use variables in instance config
+app.register_blueprint(home) #register the blue print
